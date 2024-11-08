f2824709547c8aed0e52f577a9023021cdfa2c13
alex64a <aleksamitrovcan@gmail.com>
2024-11-08 10:12:53 +0100

    Initial Commit

---
+++ AirbnbSwiftUI/AirbnbSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbSwiftUIApp.swift
+//  AirbnbSwiftUI
+//
+//  Created by Aleksa on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirbnbSwiftUITests/AirbnbSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbSwiftUITests.swift
+//  AirbnbSwiftUITests
+//
+//  Created by Aleksa on 11/8/24.
+//
+
+import Testing
+@testable import AirbnbSwiftUI
+
+struct AirbnbSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
