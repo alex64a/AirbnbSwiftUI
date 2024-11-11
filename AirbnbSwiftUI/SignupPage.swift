21ce133b88f45e6b7881367398844366cbe1ed69
alex64a <aleksamitrovcan@gmail.com>
2024-11-11 16:14:24 +0100

    Added signup page, password visibility

---
+++ AirbnbSwiftUI/SignupPage.swift
@@ -0,0 +1,18 @@
+//
+//  SignupPage.swift
+//  AirbnbSwiftUI
+//
+//  Created by Aleksa on 11/9/24.
+//
+
+import SwiftUI
+
+struct SignupPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupPage()
+}
